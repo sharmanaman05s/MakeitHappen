28e010cf7bcd920b8b8c48f5f83ee62ee508373d
sharmanaman05s <2021ucp1186@mnit.ac.in>
2024-07-06 08:20:20 +0530

    clerk to db againnnn

--- middleware.ts
+++ middleware.ts
@@ -6,10 +6,3 @@ export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 
-// import { authMiddleware } from "@clerk/nextjs";
- 
-// export default authMiddleware({});
- 
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
