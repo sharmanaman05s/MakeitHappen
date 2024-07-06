f02a51a3638dfabf6de4db9740e9a5bae4c00cc4
sharmanaman05s <2021ucp1186@mnit.ac.in>
2024-07-06 08:14:54 +0530

    clerk to db againnnn

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks(.*)"],
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// import { authMiddleware } from "@clerk/nextjs";
+ 
+// export default authMiddleware({});
+ 
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
