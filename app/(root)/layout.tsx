366aaa6831757d27b392ceb8a42159d0506e1397
sharmanaman05s <2021ucp1186@mnit.ac.in>
2024-07-08 02:46:24 +0530

    final commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Sidebar from '@/components/shared/Sidebar'
-import MobileNav from '@/components/shared/MobileNav'
+import Sidebar from 'components/shared/Sidebar'
+import MobileNav from 'components/shared/MobileNav'
+import { Toaster } from 'components/ui/toaster'
 const Layout = ({children}: {children: React.ReactNode}) => {
     return (
     <main className="root">
@@ -10,7 +11,8 @@ const Layout = ({children}: {children: React.ReactNode}) => {
           <div className="wrapper">
             {children}
           </div>
-        </div>    
+        </div> 
+        <Toaster/>   
     </main>
   )
 }
